419b299e11251af6b9a669b6464f6a6c2d311dee
ryankao78 <36420454+ryankao78@users.noreply.github.com>
2022-08-08 08:47:11 -0500

    Add files via upload

--- MeetUp_project_backend_v2/src/main/java/com/example/demo/controller/EventController.java
+++ MeetUp_project_backend_v2/src/main/java/com/example/demo/controller/EventController.java
@@ -7,11 +7,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.example.demo.dto.EventDTO;
 import com.example.demo.model.Event;
 import com.example.demo.service.EventService;
 
@@ -23,12 +29,14 @@ public class EventController {
 	@Autowired
 	private EventService eService;
 	
+	@CrossOrigin
 	@GetMapping(path = "/getAllEvents")
 	public ResponseEntity<?> getAllEvents() {
 		List<Event> eventList = eService.getAllEvents();
 		return ResponseEntity.ok(eventList);
 	}
 	
+	@CrossOrigin
 	@GetMapping(path = "/getEvent/{id}")
 	public ResponseEntity<?> getEventById(@PathVariable("id") String id) {
 		try {
@@ -37,5 +45,28 @@ public class EventController {
 		} catch (RuntimeException RE) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found", RE);
 		}
+	}
+	
+	@CrossOrigin
+	@PostMapping(path = "/insertEvent")
+	public ResponseEntity<?> createEvent(@RequestBody EventDTO eventDTO) {
+		LOGGER.info("EventController createEvent(): ");
+		Event event = eService.insertEvent(eventDTO);
+		return ResponseEntity.ok(event);
+	}
+	
+	@CrossOrigin
+	@PutMapping(path = "/updateEvent")
+	public ResponseEntity<?> updateEvent(@RequestBody EventDTO eventDTO) {
+		LOGGER.info("EventController updateEvent(): ");
+		Event event = eService.updateEvent(eventDTO);
+		return ResponseEntity.ok(event);
+	}
+	
+	@DeleteMapping(path = "/deleteEvent/{id}")
+	public ResponseEntity<?> deleteEventById(@PathVariable("id") String id) {
+		LOGGER.info("EventController deleteEventById(): ");
+		String result = eService.deleteEventById(id);
+		return ResponseEntity.ok(result);
 	}
 }
--- MeetUp_project_backend_v2/src/main/java/com/example/demo/model/Event.java
+++ MeetUp_project_backend_v2/src/main/java/com/example/demo/model/Event.java
@@ -19,6 +19,8 @@ public class Event {
 	private int joinCount;
 	private int rejectCount;
 	private Date createdAt;
+	
+	public Event() { }
 
 	public Event(String id, String creator, String title, String message, int joinCount,
 			int rejectCount, Date createdAt) {
